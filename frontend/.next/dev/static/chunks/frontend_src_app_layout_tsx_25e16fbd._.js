3867c9323bd34d08e400c94efa6d4dcb87373fc6
Chicken_Yi <sonmessia123@gmai.com>
2026-03-08 06:15:07 +0700

    chore: Add .gitignore file with common exclusions.

--- frontend/.next/dev/static/chunks/frontend_src_app_layout_tsx_25e16fbd._.js
+++ frontend/.next/dev/static/chunks/frontend_src_app_layout_tsx_25e16fbd._.js
@@ -1,8 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/frontend_src_app_globals_a7e5a063.css",
-  "static/chunks/9e883_next_dist_72742c92._.js"
+  "static/chunks/frontend_src_app_globals_a7e5a063.css"
 ],
     source: "dynamic"
 });
